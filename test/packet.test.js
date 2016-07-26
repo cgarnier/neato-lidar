187f285904ad5c3cb3d89259168a3fbcb41a61ce
Clément Garnier <cle.garnier@gmail.com>
2016-07-26 18:25:58 +0200

    measure tests

--- test/packet.test.js
+++ test/packet.test.js
@@ -1,53 +1,37 @@
 const chai = require('chai')
 const assert = chai.assert
 
-const LidarPacket = require('../lib/LidarPacket')
 const LidarMeasure = require('../lib/LidarMeasure')
 
-var sample = new Uint8Array([
-  0xFA, // fanion
-  0xB1, // index
-  0x6B, 0x4C, // speed
-  0xD8, 0x40, 0x60, 0x00,  // measure 1
-  0xD1, 0x00, 0x0B, 0x01,  // measure 2
-  0xCC, 0x00, 0xD4, 0x02,  // measure 3
-  0xC8, 0x00, 0x77, 0x02,  // measure 4
-  0x29, 0x27 // CRC
-])
-let index = 17;
-let measuresIndexes = [index * 4, index * 4 + 1, index * 4 + 2, index * 4 + 3]
-let speed = 305.671875
+let warningSample = new Uint8Array([0xD8, 0x40, 0x60, 0x00])
+let invalidSample = new Uint8Array([0xD8, 0x88, 0x60, 0x00])
+let sample = new Uint8Array([0xD8, 0x24, 0x60, 0x00])
 
-describe('LidarPacket exceptions', () => {
-  it('should raise TypeError when data is not a Uint8Array', () => {
-    assert.throws(() => new LidarPacket('0xdfdfdf'), TypeError)
+let signalStrength = 96
+let distance = 9432
+
+describe('LidarMeasure flags', () => {
+  it('should decode a warning measure when bit 2 of byte 2 is set', () => {
+    let m = new LidarMeasure(warningSample)
+    assert.isTrue(m.warning)
   })
-  it('should raise RangeError when data is not a 22 length Uint8Array', () => {
-    assert.throws(() => new LidarPacket([0xfa, 0xaa]), TypeError)
-  })
-  it('should raise an Error when it s corrupted (CRC missmatch)', () => {
-    let corrupted = sample.slice()
-    corrupted[21] = 0xAA
-    assert.throws(() => new LidarPacket(corrupted), Error)
+  it('should decode an invalid measure when bit 1 of byte 2 is set', () => {
+    let m = new LidarMeasure(invalidSample)
+    assert.isTrue(m.invalid)
   })
 })
 
-describe('LidarPacket decode', () => {
-  let lp
+describe('LidarMeasure decode', () => {
+  let lm
   before(() => {
-    lp = new LidarPacket(sample)
+    lm = new LidarMeasure(sample)
   })
 
-  it('should decode the right index', () => {
-    assert.equal(lp.index, index)
+  it('should decode the right distance', () => {
+    assert.equal(lm.distance, distance)
   })
 
-  it('should decode the right speed', () => {
-    assert.equal(lp.speed, speed)
+  it('should decode the right signal strength', () => {
+    assert.equal(lm.signalStrength, signalStrength)
   })
-
-  it('should decode the right indexes for its measures', () => {
-    [0, 1, 2, 3].forEach((i) => assert.equal(lp.measures[i].index, measuresIndexes[i]))
-  })
-
 })
